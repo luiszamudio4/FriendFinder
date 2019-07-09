2d3873ca6c8da0253289f3fd489eab4a7cb0530e
luiszamudio4 <rodriguez.of.luis@gmail.com>
2019-07-08 23:11:30 -0500

    added html and js to the files. Also installed the packages

---
+++ app/Data/friends.js
@@ -0,0 +1,55 @@
+var friendsArray = [
+    {
+        "name": "Robert",
+        "photo": "",
+        "scores": [
+            1,
+            1,
+            1,
+            5,
+            1,
+            5,
+            1,
+            1,
+            5,
+            1
+        ]
+
+    },
+    {
+        "name": "Nathaniel",
+        "photo": "",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            5,
+            1,
+            1,
+            1,
+            1,
+            5
+        ]
+
+    },
+    {
+        "name": "Grant",
+        "photo": "",
+        "scores": [
+            5,
+            5,
+            5,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+
+    }
+];
+
+module.exports = friendsArray;
